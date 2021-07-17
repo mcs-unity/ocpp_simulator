afdc6d7e5727c50e0978511844f2ad2d9e8b88ff
Nasar Eddaoui <nasar165@hotmail.com>
2021-07-17 21:38:15 +0700

    fix bug where json is stringified multiple times

--- src/app/helper/ocpp.ts
+++ src/app/helper/ocpp.ts
@@ -3,17 +3,17 @@ import { v4 } from 'uuid';
 
 function ocppReq(action: string, payload: any) {
   const message = [MessageType.CALL, v4(), action, payload];
-  return JSON.stringify(message);
+  return message;
 }
 
 function ocppRes(action: string, payload: any) {
   const message = [MessageType.CALLRESULT, action, payload];
-  return JSON.stringify(message);
+  return message;
 }
 
 function ocppError(action: string, payload: any) {
   const message = [MessageType.CALLERROR, action, payload];
-  return JSON.stringify(message);
+  return message;
 }
 
 export { ocppReq, ocppRes, ocppError };
