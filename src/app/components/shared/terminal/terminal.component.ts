506d0e4933a909f6b579576d542ea958959c48bf
Nasar Eddaoui <nasar165@hotmail.com>
2022-02-27 03:02:06 +0700

    Allow terminal to overflow with scroll

--- src/app/components/shared/terminal/terminal.component.ts
+++ src/app/components/shared/terminal/terminal.component.ts
@@ -7,13 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TerminalComponent implements OnInit {
   state = 'Pending';
-  response = {
-    id: '0001',
-    type: 'donut',
-    name: 'Cake',
-    image: { url: 'images/0001.jpg', width: 200, height: 200 },
-    thumbnail: { url: 'images/thumbnails/0001.jpg', width: 32, height: 32 },
-  };
+  response = [
+    {
+      id: '0001',
+      type: 'donut',
+      name: 'Cake',
+      image: { url: 'images/0001.jpg', width: 200, height: 200 },
+      thumbnail: { url: 'images/thumbnails/0001.jpg', width: 32, height: 32 },
+    },
+    {
+      id: '0001',
+      type: 'donut',
+      name: 'Cake',
+      image: { url: 'images/0001.jpg', width: 200, height: 200 },
+      thumbnail: { url: 'images/thumbnails/0001.jpg', width: 32, height: 32 },
+    },
+  ];
   constructor() {}
 
   ngOnInit(): void {}
